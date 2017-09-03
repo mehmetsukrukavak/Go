04f01405d21fcb8c14e1842556b9c8a657d13abb
mkavak <mkavak@mngkargo.com.tr>
2017-09-03 11:14:46 +0300

    examples

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+/*
+	for _, env := range os.Environ() {
+		fmt.Println(env)
+	}
+*/
+	var myString = "1"
+
+	number, _ := strconv.Atoi(myString)
+
+	fmt.Println("Sonuç : " +strconv.Itoa(number))
+
+
 
 }
